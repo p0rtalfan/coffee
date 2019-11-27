a7d367c3faef669083be5e238a9adcba30b1b240
p0rtalfan <55884044+p0rtalfan@users.noreply.github.com>
2019-11-27 19:14:13 +0300

    gotovo

---
+++ main.py
@@ -0,0 +1,38 @@
+import sys
+from PyQt5.QtWidgets import QApplication,\
+    QMainWindow,\
+    QTableWidgetItem
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+from PyQt5 import uic, \
+    QtCore, \
+    QtMultimedia
+import sqlite3
+
+
+# main menu form
+class MainWidget(QMainWindow):
+    # initialization
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('ui forms/main.ui', self)
+        self.setWindowTitle('Menu')
+        self.pushButton.connect(self.a())
+        self.show()
+
+
+    def a(self):
+        con = sqlite3.connect('coffee.db')
+        cur = con.cursor()
+        self.table = cur.execute("SELECT * from coffee").fetchall()
+        length = len(self.table)
+        for i in range(length):
+            self.table[i] = [str(self.table[i][0]).upper(),
+                             self.table[i][1],
+                             float(self.table[i][2].split(', ')[-1])]
+        con.close()
+        self.current_table = [] + self.table
+        self.tableWidget.setColumnCount(4)
+        self.tableWidget.setHorizontalHeaderLabels(self.table[0])
+        self.show_table(self.current_table)
+        self.show()
